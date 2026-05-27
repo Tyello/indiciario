a4b771ac1231a8af4a1e3e5566dcb7fa0db3fc9b
Marcelo Oliveira <marceloxoliveira@gmail.com>
2026-05-27 18:27:22 -0300

    Sanitize HTML templates

--- models.py
+++
@@ -1,7 +0,0 @@
-"""Compatibilidade temporária.
-
-Os modelos foram movidos para `generator.models`.
-Este wrapper mantém imports antigos funcionando durante a migração.
-"""
-
-from generator.models import *  # noqa: F401,F403
